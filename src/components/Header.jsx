7685493213c9a4416ab67a5ad824489172dfccbb
seunggyun <asg5450@gmail.com>
2024-11-29 12:25:00 +0900

    Header CSS 스타일링
    Header CSS 스타일링

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 export default function Header({ filters, filter, onFilterChange }) {
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {filters.map((value, index) => {
           return (
             <li key={index}>
               <button
+                className={`${styles.filter} ${
+                  filter === value && styles.selected
+                }`}
                 onClick={() => {
                   onFilterChange(value);
                 }}
