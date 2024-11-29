cf8ddb6b2b08109d2dd182ec88349aae7a8631e0
seunggyun <asg5450@gmail.com>
2024-11-29 11:09:03 +0900

    Todo 컴포넌트화
    삭제 버튼 기능 생성

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AddTodo from "./AddTodo";
+import Todo from "./Todo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
@@ -8,12 +9,22 @@ export default function TodoList() {
   ]);
 
   const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
 
   return (
     <section>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
