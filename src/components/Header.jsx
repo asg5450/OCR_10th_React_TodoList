ff5dca7859ce2915bfef1fe46c0a6d623b814a76
seunggyun <asg5450@gmail.com>
2024-11-29 12:13:58 +0900

    Header 생성 (필터링 기능)
    Header 생성 (필터링 기능)

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Header({ filters, filter, onFilterChange }) {
+  return (
+    <header>
+      <ul>
+        {filters.map((value, index) => {
+          return (
+            <li key={index}>
+              <button
+                onClick={() => {
+                  onFilterChange(value);
+                }}
+              >
+                {value}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+  );
+}
