cf122a15197bd8a5f02a744cdd7abb7f60966f94
seunggyun <asg5450@gmail.com>
2024-11-29 10:28:29 +0900

    Todo 추가하기 (feat.uuid)
    AddTodo 컴포넌트 생성 및 uuid 모듈 사용

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { v4 } from "uuid";
+
+export default function AddTodo(props) {
+  const { onAdd } = props;
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length == 0) return;
+    onAdd({ id: v4(), text, status: "active" });
+    setText("");
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="할 일 추가"
+        value={text}
+        onChange={handleChange}
+      />
+      <button>추가</button>
+      <p>{text}</p>
+    </form>
+  );
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import AddTodo from "./AddTodo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "1", text: "커피사기", status: "active" },
     { id: "2", text: "리액트공부하기", status: "active" },
   ]);
+
+  const handleAdd = (todo) => setTodos([...todos, todo]);
 
   return (
     <section>
@@ -13,6 +16,7 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
