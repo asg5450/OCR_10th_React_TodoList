ab1db01c59c179ca236f62069fbdd645092e3f49
seunggyun <asg5450@gmail.com>
2024-11-29 09:49:41 +0900

    TodoList Component 초안
    TodoList Component 초안

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: "1", text: "커피사기", status: "active" },
+    { id: "2", text: "리액트공부하기", status: "active" },
+  ]);
+
+  return (
+    <section>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
